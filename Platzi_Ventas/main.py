19e29786989634067e9f121c9a0538347f8c7c04
Mario Riascos <marioriascos1201@gmail.com>
2023-02-18 15:00:26 -0500

    update fix

--- Platzi_Ventas/main.py
+++ Platzi_Ventas/main.py
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 
 
 
 if __name__ == '__main__':
     pass
+=======
+clients = 'Pablo,Ricardo,'
+
+
+if __name__ == '__main__':
+    clients += 'David'
+    print(clients)
+>>>>>>> 42f71ed (Update All and Start Project)
